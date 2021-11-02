7435ed61022884136cc243e271a574cd7c8bd4ab
prathik <37804760+prmali@users.noreply.github.com>
2021-11-01 17:02:47 -0700

    Outline tests

---
+++ test/strippedMarketplace.ts
@@ -0,0 +1,23 @@
+/*
+[Alice, Bob] = [Wallet 1, Wallet 2]
+
+Alice creates store ✔
+Bob purchases product X
+
+Alice creates product w/ stock 1 ✔
+Bob creates product ✔
+
+Bob purchases product X
+Alice funds rise ✔
+Bobs funds decrease ✔
+Stock drops 1 ✔
+
+Product stock = 0 ✔
+bob purchases product X
+Bob restocks X
+Alice restocks ✔
+Product stock reflects ✔
+
+Bob purchases product ✔
+
+*/
