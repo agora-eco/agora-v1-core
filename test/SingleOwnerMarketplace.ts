7ffa39880dfd121da16149bdef27402b08c9a361
prathik <37804760+prmali@users.noreply.github.com>
2021-11-04 22:08:07 -0700

    Update SingleOwnerMarketplace.ts

--- test/SingleOwnerMarketplace.ts
+++ test/SingleOwnerMarketplace.ts
@@ -5,9 +5,9 @@ Alice creates store ✔
 Bob purchases product X
 
 Alice creates product w/ stock 1 ✔
-Bob creates product ✔
+Bob creates product X
 
-Bob purchases product X
+Bob purchases product ✔
 Alice funds rise ✔
 Bobs funds decrease ✔
 Stock drops 1 ✔
@@ -25,14 +25,16 @@ Bob purchases product ✔
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("StrippedMarketplace", () => {
+describe("SingleOwnerMarketplace", () => {
 	let accounts;
-	let StrippedMarketplace, strippedMarketplace;
+	let SingleOwnerMarketplace, singleOwnerMarketplace;
 
 	before(async () => {
 		accounts = await ethers.getSigners();
-		StrippedMarketplace = ethers.getContractFactory("StrippedMarketplace");
-		strippedMarketplace = await StrippedMarketplace.deploy(
+		SingleOwnerMarketplace = await ethers.getContractFactory(
+			"SingleOwnerMarketplace"
+		);
+		singleOwnerMarketplace = await SingleOwnerMarketplace.deploy(
 			"RBM",
 			"Rich Boy Marketplace"
 		);
@@ -40,20 +42,121 @@ describe("StrippedMarketplace", () => {
 
 	it("Store Creation", async () => {
 		const [alice, bob] = accounts;
-		const validCreateProductTxn = await strippedMarketplace
+		const validCreateProductTxn = await singleOwnerMarketplace
 			.connect(alice)
-			.create("MS", "Milkshake", 0.1, 4);
+			.create("MS", "Milkshake", 0.1, 1);
 		await validCreateProductTxn.wait();
 
 		try {
-			const invalidCreateProductTxn = await strippedMarketplace
+			const invalidCreateProductTxn = await singleOwnerMarketplace
 				.connect(bob)
 				.create("BMS", "Bad Milkshake", 0.1, 1);
 			await invalidCreateProductTxn.wait();
-		} catch (err) {
-			expect(err.message).to.equal(
+		} catch (error) {
+			expect(error.message).to.equal(
 				"VM Exception while processing transaction: reverted with reason string: 'not owner'"
 			);
 		}
+	});
+
+	it("Catalog Inspection", async () => {
+		const [alice, bob] = accounts;
+
+		expect(await singleOwnerMarketplace.inspect("MS")).to.eql([
+			true, // exists
+			ethers.BigNumber.from(0.1), // price
+			"Milkshake", // name
+			1, // quantity
+		]);
+
+		try {
+			await singleOwnerMarketplace.inspect("BMS");
+		} catch (error) {
+			expect(error.message).to.equal(
+				"VM Exception while processing transaction: reverted with reason string: 'product dne'"
+			);
+		}
+	});
+
+	it("Purchase", async () => {
+		const [alice, bob] = accounts;
+
+		try {
+			const bobPurchaseTxn = await singleOwnerMarketplace
+				.connect(bob)
+				.purchase("MS", 10, {
+					value: (10 * 0.1 * 10 ** 18).toString(),
+				});
+			await bobPurchaseTxn.wait();
+		} catch (error) {
+			expect(error.message).to.equal(
+				"VM Exception while processing transaction: reverted with reason string: 'insufficient stock'"
+			);
+		}
+
+		try {
+			const bobPurchaseTxn = await singleOwnerMarketplace
+				.connect(bob)
+				.purchase("MS", 1, {
+					value: 0,
+				});
+			await bobPurchaseTxn.wait();
+		} catch (error) {
+			expect(error.message).to.equal(
+				"VM Exception while processing transaction: reverted with reason string: 'insufficient funds'"
+			);
+		}
+
+		const bobPurchaseTxn = await singleOwnerMarketplace
+			.connect(bob)
+			.purchase("MS", 1, {
+				value: (0.1 * 10 ** 18).toString(),
+			});
+		await bobPurchaseTxn.wait();
+
+		expect(await singleOwnerMarketplace.inspect("MS")).to.eql([
+			true, // exists
+			ethers.BigNumber.from(0.1), // price
+			"Milkshake", // name
+			0, // quantity
+		]);
+
+		// check for funds decreasing
+
+		try {
+			const bobPurchaseTxn2 = await singleOwnerMarketplace
+				.connect(bob)
+				.purchase("MS", 1, {
+					value: 0,
+				});
+			await bobPurchaseTxn2.wait();
+		} catch (error) {
+			expect(error.message).to.equal(
+				"VM Exception while processing transaction: reverted with reason string: 'product oos'"
+			);
+		}
+	});
+
+	it("Restock", async () => {
+		try {
+			const bobRestockTxn = await singleOwnerMarketplace
+				.connect(bob)
+				.restock("MS", 10, true);
+			await bobRestockTxn.wait();
+		} catch (error) {
+			expect(error.message).to.equal(
+				"VM Exception while processing transaction: reverted with reason string: 'not owner'"
+			);
+		}
+
+		const aliceRestockTxn = await singleOwnerMarketplace.restock("MS", 5);
+		await aliceRestockTxn.wait();
+
+		expect(await singleOwnerMarketplace.inspect("MS")).to.eql([
+			true, // exists
+			ethers.BigNumber.from(0.1), // price
+			"Milkshake", // name
+			5, // quantity
+		]);
 	});
 });
