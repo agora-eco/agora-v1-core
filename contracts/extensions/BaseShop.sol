baeda91ae6ae3ac2a249e89cf5724bece49c0ee3
prathik <37804760+prmali@users.noreply.github.com>
2021-11-07 10:12:09 -0800

    decoupling in progress

---
+++ contracts/extensions/BaseShop.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BaseShop {
+    string public name;
+    string public symbol;
+    mapping(string => Product) internal _catalog;
+
+    struct Product {
+        bool exists;
+        uint256 price;
+        string name;
+        uint256 quantity;
+    }
+
+    event Create(
+        string productCode,
+        string name,
+        uint256 price,
+        uint256 quantity,
+        address indexed initiator
+    );
+
+    event Adjust(
+        string productCode,
+        string name,
+        uint256 price,
+        address indexed initiator
+    );
+
+    event Purchase(
+        string name,
+        uint256 quantity,
+        uint256 value,
+        address indexed initiator
+    );
+
+    event Restock(
+        string productCode,
+        string name,
+        uint256 quantity,
+        bool forced,
+        address indexed initiator
+    );
+
+    modifier productNotExist(string memory _productCode) {
+        require(
+            _catalog[_productCode].exists == false,
+            "product already exists"
+        );
+        _;
+    }
+
+    function _establish(string memory _symbol, string memory _name) internal {
+        symbol = _symbol;
+        name = _name;
+    }
+
+    function _create(
+        string memory _productCode,
+        string memory _name,
+        uint256 _price,
+        uint256 _quantity
+    ) internal productNotExist(_productCode) {
+        _catalog[_productCode] = Product(true, _price, _name, _quantity);
+
+        emit Create(_productCode, _name, _price, _quantity, tx.origin);
+    }
+
+    function _adjust(
+        string memory _productCode,
+        string memory _name,
+        uint256 _price
+    ) internal {
+        require(_catalog[_productCode].exists == true, "product dne");
+        _catalog[_productCode] = Product(
+            true,
+            _price,
+            _name,
+            _catalog[_productCode].quantity
+        );
+
+        emit Adjust(_productCode, _name, _price, tx.origin);
+    }
+
+    function _purchase(
+        string memory _productCode,
+        uint256 _quantity,
+        address _owner
+    ) internal productNotExist(_productCode) {
+        Product memory _product = _catalog[_productCode];
+
+        require(_quantity > 0, "invalid quantity");
+        require(_product.quantity > 0, "product oos");
+        require(_product.quantity >= _quantity, "insufficient stock");
+        require(
+            _quantity * _product.price * 10**18 <= msg.value,
+            "insufficient funds"
+        );
+
+        _product.quantity -= _quantity;
+        _catalog[_productCode] = _product;
+
+        payable(_owner).transfer(_product.price);
+
+        emit Purchase(
+            _product.name,
+            _quantity,
+            _product.price * _quantity,
+            tx.origin
+        );
+    }
+
+    function _restock(string memory _productCode, uint256 _quantity)
+        internal
+        productNotExist(_productCode)
+    {
+        Product memory _product = _catalog[_productCode];
+
+        _product.quantity += _quantity;
+        _catalog[_productCode] = _product;
+
+        emit Restock(_productCode, _product.name, _quantity, false, tx.origin);
+    }
+
+    function _restock(
+        string memory _productCode,
+        uint256 _quantity,
+        bool _forced
+    ) internal productNotExist(_productCode) {
+        Product memory _product = _catalog[_productCode];
+
+        if (_forced == true) {
+            _product.quantity = _quantity;
+        } else {
+            _product.quantity += _quantity;
+        }
+        _catalog[_productCode] = _product;
+
+        emit Restock(
+            _productCode,
+            _product.name,
+            _quantity,
+            _forced,
+            tx.origin
+        );
+    }
+
+    function _inspect(string calldata _productCode)
+        internal
+        view
+        returns (Product memory)
+    {
+        Product memory _product = _catalog[_productCode];
+
+        require(_product.exists == true, "product dne");
+
+        return _product;
+    }
+}
--- contracts/extensions/SingleOwnerShop.sol
+++ contracts/extensions/SingleOwnerShop.sol
@@ -1,70 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {BaseShop} from "./BaseShop.sol";
+
 /**
     Fundamental shop with stripped functionality. Will serve as basis for future iterations of marketplace
 
  */
 
-contract SingleOwnerShop {
-    address owner;
-    string public name;
-    string public symbol;
-    struct Product {
-        bool exists;
-        uint256 price;
-        string name;
-        uint256 quantity;
-    }
-    mapping(string => Product) _catalog;
-
-    event Create(
-        string productCode,
-        string name,
-        uint256 price,
-        uint256 quantity,
-        address indexed initiator
-    );
-
-    event Adjust(
-        string productCode,
-        string name,
-        uint256 price,
-        address indexed initiator
-    );
-
-    event Purchase(
-        string name,
-        uint256 quantity,
-        uint256 value,
-        address indexed initiator
-    );
-
-    event Restock(
-        string productCode,
-        string name,
-        uint256 quantity,
-        bool forced,
-        address indexed initiator
-    );
-
-    modifier productNotExist(string memory _productCode) {
-        require(
-            _catalog[_productCode].exists == false,
-            "product already exists"
-        );
-        _;
-    }
+contract SingleOwnerShop is BaseShop {
+    address public owner;
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "not owner");
+        require(tx.origin == owner, "not owner");
         _;
     }
 
     constructor(string memory _symbol, string memory _name) {
-        symbol = _symbol;
-        name = _name;
-        owner = msg.sender;
+        _establish(_symbol, _name);
+        owner = tx.origin;
     }
 
     function create(
@@ -73,9 +27,7 @@ contract SingleOwnerShop {
         uint256 _price,
         uint256 _quantity
     ) public productNotExist(_productCode) onlyOwner {
-        _catalog[_productCode] = Product(true, _price, _name, _quantity);
-
-        emit Create(_productCode, _name, _price, _quantity, msg.sender);
+        _create(_productCode, _name, _price, _quantity);
     }
 
     function adjust(
@@ -83,15 +35,7 @@ contract SingleOwnerShop {
         string memory _name,
         uint256 _price
     ) public onlyOwner {
-        require(_catalog[_productCode].exists == true, "product dne");
-        _catalog[_productCode] = Product(
-            true,
-            _price,
-            _name,
-            _catalog[_productCode].quantity
-        );
-
-        emit Adjust(_productCode, _name, _price, msg.sender);
+        _adjust(_productCode, _name, _price);
     }
 
     function purchase(string memory _productCode, uint256 _quantity)
@@ -99,27 +43,7 @@ contract SingleOwnerShop {
         payable
         productNotExist(_productCode)
     {
-        Product memory _product = _catalog[_productCode];
-
-        require(_quantity > 0, "invalid quantity");
-        require(_product.quantity > 0, "product oos");
-        require(_product.quantity >= _quantity, "insufficient stock");
-        require(
-            _quantity * _product.price * 10**18 <= msg.value,
-            "insufficient funds"
-        );
-
-        _product.quantity -= _quantity;
-        _catalog[_productCode] = _product;
-
-        payable(owner).transfer(_product.price);
-
-        emit Purchase(
-            _product.name,
-            _quantity,
-            _product.price * _quantity,
-            msg.sender
-        );
+        _purchase(_productCode, _quantity, owner);
     }
 
     function restock(string memory _productCode, uint256 _quantity)
@@ -127,12 +51,7 @@ contract SingleOwnerShop {
         onlyOwner
         productNotExist(_productCode)
     {
-        Product memory _product = _catalog[_productCode];
-
-        _product.quantity += _quantity;
-        _catalog[_productCode] = _product;
-
-        emit Restock(_productCode, _product.name, _quantity, false, msg.sender);
+        _restock(_productCode, _quantity);
     }
 
     function restock(
@@ -140,22 +59,7 @@ contract SingleOwnerShop {
         uint256 _quantity,
         bool _forced
     ) public onlyOwner productNotExist(_productCode) {
-        Product memory _product = _catalog[_productCode];
-
-        if (_forced == true) {
-            _product.quantity = _quantity;
-        } else {
-            _product.quantity += _quantity;
-        }
-        _catalog[_productCode] = _product;
-
-        emit Restock(
-            _productCode,
-            _product.name,
-            _quantity,
-            _forced,
-            msg.sender
-        );
+        _restock(_productCode, _quantity, _forced);
     }
 
     function inspect(string calldata _productCode)
@@ -163,10 +67,6 @@ contract SingleOwnerShop {
         view
         returns (Product memory)
     {
-        Product memory _product = _catalog[_productCode];
-
-        require(_product.exists == true, "product dne");
-
-        return _product;
+        _inspect(_productCode);
     }
 }
