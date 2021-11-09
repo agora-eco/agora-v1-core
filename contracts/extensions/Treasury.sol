ed578d719c411ab4eb965220ae06e18348f2244a
prathik <37804760+prmali@users.noreply.github.com>
2021-11-09 13:25:39 -0800

    Update Treasury.sol

--- contracts/extensions/Treasury.sol
+++ contracts/extensions/Treasury.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract Treasury {
+    uint256 balance = 0;
+
     /*
      */
     function deposit() external {}
 
     /*
      */
-    function withdraw() external {}
+    function withdraw(uint256 amount) external {
+        payable(tx.origin).transfer(amount);
+    }
 }
