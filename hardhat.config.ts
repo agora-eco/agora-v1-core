9ea361a570582d7b5a8bbcdfacbeb66bbd6afa3b
prathik <37804760+prmali@users.noreply.github.com>
2021-11-10 21:04:21 -0800

    packages + hardhat config upgrades

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (args, hre) => {
 	const accounts = await hre.ethers.getSigners();
 
 	for (const account of accounts) {
@@ -16,6 +19,12 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+export default {
 	solidity: "0.8.4",
+	typechain: {
+		outDir: "src/types",
+		target: "ethers-v5",
+		alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+		externalArtifacts: ["node_modules/@openzeppelin/*/build/*.json"], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+	},
 };
