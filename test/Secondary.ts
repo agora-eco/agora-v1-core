47e1be8af831459c51bfb6ef92bb6a817b7a3a4d
simonfei123 <tfei@ucsd.edu>
2022-02-25 17:04:34 -0800

    Push to debug

--- test/Secondary.ts
+++ test/Secondary.ts
@@ -3,26 +3,50 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 import { Signer } from "ethers";
+import { MarketFactory } from "../src/Types/MarketFactory";
+import { Market } from "../src/Types/Market";
 import { Secondary } from "../src/Types/Secondary";
 
 describe('Secondary', () => {
     let alice: Signer, bob: Signer;
     let secondary: Secondary;
+	let marketFactory: MarketFactory;
+	let market: Market;
     before(async() => {
 		[alice, bob] = await ethers.getSigners();
 	});
 
-    describe('Deploy market', ()=>{
-        it('deploy', async()=>{
-            const Secondary = await ethers.getContractFactory("Secondary");
-            secondary = await Secondary.deploy();
-            const initializeTxn = await secondary.initialize(
-				"RBM",
-				"Rich Boy Market"
+	describe("Deploy MarketFactory", () => {
+		it("deploy", async () => {
+			const MarketFactory = await ethers.getContractFactory("MarketFactory");
+			marketFactory = await MarketFactory.deploy(
+				await alice.getAddress()
 			);
-			await initializeTxn.wait();
-        });
-    });
+		});
+	});
+	
+	describe("Initialize Proxies", () => {
+		it("Deploy Default Market", async () => {
+			const Market = await ethers.getContractFactory("Market");
+			market = await Market.deploy();
+		});
+		it("Deploy Secondary Market", async () => {
+			const Secondary = await ethers.getContractFactory("Secondary");
+			secondary = await Secondary.deploy();
+		});
+		it("Add Default Market Extension", async () => {
+			const addDefaultMarketExtensionTx = await marketFactory
+				.connect(alice)
+				.addExtension("Default Market", market.address);
+			await addDefaultMarketExtensionTx.wait();
+		});
+		it("Add Secondary Market Extension", async () => {
+			const addSecondaryMarketExtensionTx = await marketFactory
+				.connect(alice)
+				.addExtension("Secondary Market", secondary.address);
+			await addSecondaryMarketExtensionTx.wait();
+		});
+	});
 
     describe("Establish catalog", () => {
 		it("owner create product", async () => {
